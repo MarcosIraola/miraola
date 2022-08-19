dad8a58ce7440424a44f37ea29f9ee37dbcb4e1d
MarcosIraola <iraolamarcos@gmail.com>
2022-08-19 00:13:13 -0300

    typewriter effect

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import styles from './homepage.module.css'
+import TypingComponent from '../Typewrite/TypingComponent';
+import '../Typewrite/typing-component.css';
 
 const Homepage = () => {
     const { t } = useTranslation();
+    
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>{t('home.hello-world')} <br/> {t('home.description')}</h1>
+            {/* <h1 className={styles.title}>{t('home.hello-world')} <br/> {t('home.description')}</h1> */}
+            <TypingComponent
+                classString=""
+                text1={t('home.hello-world')}
+                // text2={t('home.description')}
+                typingContentElementId="typing-write"
+                // styles={TypingComponentStyles}
+            />
         </div>
     )
 }
