688b557b64751ba7a609b8b03a891c4144a0bda8
MarcosIraola <iraolamarcos@gmail.com>
2022-08-19 11:54:40 -0300

    changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Navbar } from './Navbar/Navbar';
-export { default as Homepage } from './Homepage/Homepage';
+export { default as Navbar } from './Navbar';
+export { default as Homepage } from './Homepage';
 export { default as PageNotFound } from './PageNotFound';
